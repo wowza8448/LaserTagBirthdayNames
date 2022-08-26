e5a570348f24f79012083ccaaa11c7d86e6e4228
wowza8448 <71194678+wowza8448@users.noreply.github.com>
2022-08-25 17:26:54 -0700

    Update App.py

--- App.py
+++ App.py
@@ -1,5 +1,4 @@
 from tkinter import *
-from tkhtmlview import HTMLLabel
 import webbrowser
 
 root = Tk()
@@ -9,87 +8,79 @@ room.set(1)
 v = IntVar()
 v.set(1)
 
+happy = Entry(root, text="Happy Birthday")
+happy.insert(END, 'Happy Birthday')
+happy.pack()
 
-def partyFunction(videoName):	
-	if room.get() == 1:
-	#case 1 if blue
-		f = open('blue.html','w')
-		message = """<html><head><link rel="stylesheet" href="Stylea.css"></head><body><img src="bg.png"><h1>""" + happy.get() + """</h1><video autoplay muted loop id="myVideo"><source src=""" + videoName + """ type="video/mp4">Video not found</video><div class="innertext">""" + e.get() + """</div></body></html>"""
-		f.write(message)
-		f.close()
-		webbrowser.open_new_tab('blue.html')
-	#case 2 if red
-	elif room.get() == 2:
-		f = open('red.html','w')
-		message = """<html><head><link rel="stylesheet" href="Styleb.css"></head><body><img src="bg.png"><h1>""" + happy.get() + """</h1><video autoplay muted loop id="myVideo"><source src=""" + videoName + """ type="video/mp4">Video not found</video><div class="innertext">""" + e.get() + """</div></body></html>"""
-		f.write(message)
-		f.close()
-		webbrowser.open_new_tab('red.html')
-	#case 3 if yellow
-	elif room.get() == 3:
-		f = open('yellow.html','w')
-		message = """<html><head><link rel="stylesheet" href="Stylec.css"></head><body><img src="bg.png"><h1>""" + happy.get() + """</h1><video autoplay muted loop id="myVideo"><source src=""" + videoName + """ type="video/mp4">Video not found</video><div class="innertext">""" + e.get() + """</div></body></html>"""
-		f.write(message)
-		f.close()
-		webbrowser.open_new_tab('yellow.html')
-	#case 4 if orange
-	elif room.get() == 4:
-		f = open('orange.html','w')
-		message = """<html><head><link rel="stylesheet" href="Styled.css"></head><body><img src="bg.png"><h1>""" + happy.get() + """</h1><video autoplay muted loop id="myVideo"><source src=""" + videoName + """ type="video/mp4">Video not found</video><div class="innertext">""" + e.get() + """</div></body></html>"""
-		f.write(message)
-		f.close()
-		webbrowser.open_new_tab('orange.html')
+e = Entry(root, width=15)
+e.pack(ipady=8)
 
-def oldFunction(videoName):	
-	if room.get() == 1:
-	#case 1 if blue
-		f = open('blue.html','w')
-		message = """<html><head><link rel="stylesheet" href="Style.css"></head><body><img src="bg.png"><h1>""" + happy.get() + """</h1><video autoplay muted loop id="myVideo"><source src=""" + videoName + """ type="video/mp4">Video not found</video><div class="innertext">""" + e.get() + """</div></body></html>"""
-		f.write(message)
-		f.close()
-		webbrowser.open_new_tab('blue.html')
-	#case 2 if red
-	elif room.get() == 2:
-		f = open('red.html','w')
-		message = """<html><head><link rel="stylesheet" href="Style1.css"></head><body><img src="bg.png"><h1>""" + happy.get() + """</h1><video autoplay muted loop id="myVideo"><source src="movie.mp4" type="video/mp4">Video not found</video><div class="innertext">""" + e.get() + """</div></body></html>"""
-		f.write(message)
-		f.close()
-		webbrowser.open_new_tab('red.html')
-	#case 3 if yellow
-	elif room.get() == 3:
-		f = open('yellow.html','w')
-		message = """<html><head><link rel="stylesheet" href="Style2.css"></head><body><img src="bg.png"><h1>""" + happy.get() + """</h1><video autoplay muted loop id="myVideo"><source src="movie.mp4" type="video/mp4">Video not found</video><div class="innertext">""" + e.get() + """</div></body></html>"""
-		f.write(message)
-		f.close()
-		webbrowser.open_new_tab('yellow.html')
-	#case 4 if orange
-	elif room.get() == 4:
-		f = open('orange.html','w')
-		message = """<html><head><link rel="stylesheet" href="Style3.css"></head><body><img src="bg.png"><h1>""" + happy.get() + """</h1><video autoplay muted loop id="myVideo"><source src="movie.mp4" type="video/mp4">Video not found</video><div class="innertext">""" + e.get() + """</div></body></html>"""
-		f.write(message)
-		f.close()
-		webbrowser.open_new_tab('orange.html')
+def getVariables():
+    roomColor = ""
+    if room.get() == 1:
+        roomColor = "Blue"
+    elif room.get() == 2:
+        roomColor = "Red"
+    elif room.get() == 3:
+        roomColor = "Yellow"
+    else:
+        roomColor = "Orange"
+    return f"{roomColor}.html"
+
+def getColor():
+    color = getVariables()
+    color = color.replace('.html', '')
+    return color
+
+def getVideo():
+    if v.get() == 2:
+        videoName = "fortnite.mp4"
+    elif v.get() == 3:
+        videoName = "harry.mp4"
+    elif v.get() == 4:
+        videoName = "pokemon.mp4"  
+    elif v.get() == 5:
+        videoName = "minecraft.mp4"
+    elif v.get() == 1:
+        videoName = "movie.mp4"
+    return videoName
+
+def getCSS():
+    css = ""
+    color = getColor()
+    video = getVideo()
+    if color == "Blue" and video != "movie.mp4":
+        css = "Stylea.css"
+    elif color == "Red" and video != "movie.mp4":
+        css = "Styleb.css"
+    elif color == "Yellow" and video != "movie.mp4":
+        css = "Stylec.css"
+    elif color == "Orange" and video != "movie.mp4":
+        css = "Styled.css"
+    elif color == "Blue":
+        css = "Style"
+    elif color == "Red":
+        css = "Style1"
+    elif color == "Yellow":
+        css = "Style2"
+    else:
+        css = "Style3"
+    return css
 
 
-def clicked(value):
-	myLabel = Label(root, text=room.get())
-	if v.get() == 2:
-		videoName = "fortnite.mp4"
-		partyFunction(videoName)
-	elif v.get() == 3:
-		videoName = "harry.mp4"
-		partyFunction(videoName)
-	elif v.get() == 4:
-		videoName = "pokemon.mp4"
-		partyFunction(videoName)
-	elif v.get() == 5:
-		videoName = "minecraft.mp4"
-		partyFunction(videoName)
-	elif v.get() == 1:
-		videoName = "movie.mp4"
-		oldFunction(videoName)
-	
-	
+def openWebpage():
+    roomColor = getVariables()
+    file = open(roomColor,'w')
+    message = f"""<html><head><link rel='stylesheet' href='{getCSS()}'></head><body><img src='bg.png'>
+    <h1>{happy.get()}</h1><video autoplay muted loop id='myVideo'>
+    <source src={getVideo()} type='video/mp4'>Video not found</video>
+    <div class='innertext'>{e.get()}</div></body></html>"""
+    file.write(message)
+    file.close()
+    try:
+        webbrowser.get('chrome').open(roomColor)
+    except:
+        webbrowser.get('windows-default').open(roomColor)
 
 Radiobutton(root, text="Blue", variable=room, value=1).pack(side = "left")
 Radiobutton(root, text="Red", variable=room, value=2).pack(side = "left")
@@ -102,16 +93,7 @@ Radiobutton(root, text="Harry Potter", variable=v, value=3).pack(side = "right")
 Radiobutton(root, text="Pokemon", variable=v, value=4).pack(side = "bottom")
 Radiobutton(root, text="Minecraft", variable=v, value=5).pack(side = "bottom")
 
-happy = Entry(root, text="Happy Birthday")
-happy.insert(END, 'Happy Birthday')
-
-happy.pack()
-
-e = Entry(root, width=15)
-e.pack(ipady=8)
-
-myButton = Button(root, text="Make A Party", command=lambda: clicked(room.get()))
+myButton = Button(root, text="Make A Party", command=lambda: openWebpage())
 myButton.pack(side=TOP)
-
 
 root.mainloop()
